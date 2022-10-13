f95333a94ba5d7c880ef49ee83b6d51e0d1c4971
Chris Tseng <samfire5200@gmail.com>
2022-10-13 22:42:54 +0800

    43. Domain core module: Adding Aggregate Root, Entity and Valueobject

--- food-ordering-system/payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/entity/Payment.java
+++ food-ordering-system/payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/entity/Payment.java
@@ -14,105 +14,104 @@ import java.util.UUID;
 
 public class Payment extends AggregateRoot<PaymentId> {
 
-    private final OrderId orderId;
-    private final CustomerId customerId;
-    private final Money price;
+  private final OrderId orderId;
+  private final CustomerId customerId;
+  private final Money price;
 
+  private PaymentStatus paymentStatus;
+  private ZonedDateTime createdAt;
+
+  public void initializePayment() {
+    setId(new PaymentId(UUID.randomUUID()));
+    createdAt = ZonedDateTime.now(ZoneId.of("UTC"));
+  }
+
+  public void validatePayment(List<String> failureMessages) {
+    if (price == null || !price.isGreaterThanZero()) {
+      failureMessages.add("Total price must be greater than zero!");
+    }
+  }
+
+  public void updateStatus(PaymentStatus paymentStatus) {
+    this.paymentStatus = paymentStatus;
+  }
+
+  private Payment(Builder builder) {
+    setId(builder.paymentId);
+    orderId = builder.orderId;
+    customerId = builder.customerId;
+    price = builder.price;
+    paymentStatus = builder.paymentStatus;
+    createdAt = builder.createdAt;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public OrderId getOrderId() {
+    return orderId;
+  }
+
+  public CustomerId getCustomerId() {
+    return customerId;
+  }
+
+  public Money getPrice() {
+    return price;
+  }
+
+  public PaymentStatus getPaymentStatus() {
+    return paymentStatus;
+  }
+
+  public ZonedDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public static final class Builder {
+    private PaymentId paymentId;
+    private OrderId orderId;
+    private CustomerId customerId;
+    private Money price;
     private PaymentStatus paymentStatus;
     private ZonedDateTime createdAt;
 
-    public void initializePayment() {
-        setId(new PaymentId(UUID.randomUUID()));
-        createdAt = ZonedDateTime.now(ZoneId.of("UTC"));
+    private Builder() {
     }
 
-    public void validatePayment(List<String> failureMessages) {
-        if (price == null || !price.isGreaterThanZero()) {
-            failureMessages.add("Total price must be greater than zero!");
-        }
+    public Builder paymentId(PaymentId val) {
+      paymentId = val;
+      return this;
     }
 
-    public void updateStatus(PaymentStatus paymentStatus) {
-        this.paymentStatus = paymentStatus;
+    public Builder orderId(OrderId val) {
+      orderId = val;
+      return this;
     }
 
-    private Payment(Builder builder) {
-        setId(builder.paymentId);
-        orderId = builder.orderId;
-        customerId = builder.customerId;
-        price = builder.price;
-        paymentStatus = builder.paymentStatus;
-        createdAt = builder.createdAt;
+    public Builder customerId(CustomerId val) {
+      customerId = val;
+      return this;
     }
 
-    public static Builder builder() {
-        return new Builder();
+    public Builder price(Money val) {
+      price = val;
+      return this;
     }
 
-
-    public OrderId getOrderId() {
-        return orderId;
+    public Builder paymentStatus(PaymentStatus val) {
+      paymentStatus = val;
+      return this;
     }
 
-    public CustomerId getCustomerId() {
-        return customerId;
+    public Builder createdAt(ZonedDateTime val) {
+      createdAt = val;
+      return this;
     }
 
-    public Money getPrice() {
-        return price;
+    public Payment build() {
+      return new Payment(this);
     }
-
-    public PaymentStatus getPaymentStatus() {
-        return paymentStatus;
-    }
-
-    public ZonedDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public static final class Builder {
-        private PaymentId paymentId;
-        private OrderId orderId;
-        private CustomerId customerId;
-        private Money price;
-        private PaymentStatus paymentStatus;
-        private ZonedDateTime createdAt;
-
-        private Builder() {
-        }
-
-        public Builder paymentId(PaymentId val) {
-            paymentId = val;
-            return this;
-        }
-
-        public Builder orderId(OrderId val) {
-            orderId = val;
-            return this;
-        }
-
-        public Builder customerId(CustomerId val) {
-            customerId = val;
-            return this;
-        }
-
-        public Builder price(Money val) {
-            price = val;
-            return this;
-        }
-
-        public Builder paymentStatus(PaymentStatus val) {
-            paymentStatus = val;
-            return this;
-        }
-
-        public Builder createdAt(ZonedDateTime val) {
-            createdAt = val;
-            return this;
-        }
-
-        public Payment build() {
-            return new Payment(this);
-        }
-    }
+  }
 }
