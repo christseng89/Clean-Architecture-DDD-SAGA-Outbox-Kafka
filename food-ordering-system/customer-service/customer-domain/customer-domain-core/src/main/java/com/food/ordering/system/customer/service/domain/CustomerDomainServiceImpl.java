1cbae32bb00cba70b98c0bfd8b5c940daf88545a
Chris Tseng <samfire5200@gmail.com>
2022-10-31 16:07:28 +0800

    93. Implementing Customer Service modules

--- food-ordering-system/customer-service/customer-domain/customer-domain-core/src/main/java/com/food/ordering/system/customer/service/domain/CustomerDomainServiceImpl.java
+++ food-ordering-system/customer-service/customer-domain/customer-domain-core/src/main/java/com/food/ordering/system/customer/service/domain/CustomerDomainServiceImpl.java
@@ -7,13 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
+import static com.food.ordering.system.domain.DomainConstants.UTC;
+
 @Slf4j
 public class CustomerDomainServiceImpl implements CustomerDomainService {
 
   public CustomerCreatedEvent validateAndInitiateCustomer(Customer customer) {
     //Any Business logic required to run for a customer creation
     log.info("Customer with id: {} is initiated", customer.getId().getValue());
-    return new CustomerCreatedEvent(customer, ZonedDateTime.now(ZoneId.of("UTC")));
+    return new CustomerCreatedEvent(customer, ZonedDateTime.now(ZoneId.of(UTC)));
   }
 }
 
