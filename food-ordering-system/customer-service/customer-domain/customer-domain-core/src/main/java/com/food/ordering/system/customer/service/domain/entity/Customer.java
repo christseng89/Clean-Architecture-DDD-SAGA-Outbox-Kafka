95fbe7cde61cad2b7c67aa0305d947e1c14ba959
Chris Tseng <samfire5200@gmail.com>
2023-05-15 01:17:54 +0800

    Customer

--- food-ordering-system/customer-service/customer-domain/customer-domain-core/src/main/java/com/food/ordering/system/customer/service/domain/entity/Customer.java
+++ food-ordering-system/customer-service/customer-domain/customer-domain-core/src/main/java/com/food/ordering/system/customer/service/domain/entity/Customer.java
@@ -3,6 +3,8 @@ package com.food.ordering.system.customer.service.domain.entity;
 import com.food.ordering.system.domain.entity.AggregateRoot;
 import com.food.ordering.system.domain.valueobject.CustomerId;
 
+import java.util.Objects;
+
 public class Customer extends AggregateRoot<CustomerId> {
   private final String username;
   private final String firstName;
@@ -25,6 +27,20 @@ public class Customer extends AggregateRoot<CustomerId> {
 
   public String getLastName() {
     return lastName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    Customer customer = (Customer) o;
+    return Objects.equals(username, customer.username) && Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), username, firstName, lastName);
   }
 }
 
