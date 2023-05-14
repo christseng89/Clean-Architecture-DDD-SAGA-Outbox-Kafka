36a27802dffda1d7bc5b28a110fe926dbbedcd63
Chris Tseng <samfire5200@gmail.com>
2023-05-15 02:18:03 +0800

    Order Part III

--- food-ordering-system/order-service/order-domain/order-domain-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/scheduler/payment/PaymentOutboxScheduler.java
+++ food-ordering-system/order-service/order-domain/order-domain-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/scheduler/payment/PaymentOutboxScheduler.java
@@ -39,7 +39,7 @@ public class PaymentOutboxScheduler implements OutboxScheduler {
         SagaStatus.STARTED,
         SagaStatus.COMPENSATING);
 
-    if (outboxMessagesResponse.isPresent() && !outboxMessagesResponse.isEmpty()) {
+    if (outboxMessagesResponse.isPresent() && !outboxMessagesResponse.get().isEmpty()) {
       List<OrderPaymentOutboxMessage> outboxMessages = outboxMessagesResponse.get();
       log.info("Received {} OrderPaymentOutboxMessage with ids: {}, sending to message bus!",
         outboxMessages.size(),
